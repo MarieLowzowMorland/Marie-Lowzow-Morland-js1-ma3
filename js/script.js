872a2c522bce5be1e65248e1f536e749384173db
Marie Lowzow Morland <marie@morland.no>
2021-02-02 22:11:28 +0100

    created a call to an API and show the results in HTML

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Question 1
+/* Convert the function below to an arrow function:
+function division(a, b) {    
+    return a % b;
+}
+*/
+const division = (a, b) => a % b;
+
+// Question 2
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating"
+
+const gameToHTML = (game) => `<div>
+    <h2>${game.name}</h2>
+    <dl>
+        <dt>Rating</dt>
+        <dd>${game.rating}</dd>
+        <dt>Number of tags</dt>
+        <dd>${game.tags.length}</dd>
+    </dl>
+</div>`
+
+async function getGames() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const games = data.results;
+
+        document.querySelector(".games").innerHTML = games
+            .slice(0,8)
+            .map(gameToHTML)
+            .join("");
+    } catch (exception) {
+
+    }
+}
+getGames();
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+//https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dl
